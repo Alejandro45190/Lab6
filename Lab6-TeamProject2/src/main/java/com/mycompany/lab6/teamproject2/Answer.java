53e8fef538bf4c89bdb520a84a72dafa5b61d4bf
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-23 20:26:33 -0500

    issue fixed

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
@@ -3,17 +3,10 @@ package com.mycompany.lab6.teamproject2;
 
 public class Answer {
     private String text;
-<<<<<<< HEAD
     private int questionId;
 
     @Override
     public String toString() {
         return "Text: " + text + ", questionId: " + questionId;
-=======
-
-    @Override
-    public String toString() {
-        return "Text:" + text;
->>>>>>> b7e8c91fc171862a92153f153799acd9a3ad17f9
     }
 }
