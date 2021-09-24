458bf822edcbc2f0a2165c4037be38104d7a4d8f
Thomas Kolar <thomas.kolar@go.tarleton.edu>
2021-09-23 20:25:15 -0500

    File output added

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
@@ -1,7 +1,6 @@
 package com.mycompany.lab6.teamproject2;
 
 public class Question {
-<<<<<<< HEAD
     private int id;
     private String text;
 
@@ -9,13 +8,5 @@ public class Question {
     public String toString() {
         return "ID: " + id + ", text: " + text;
     }
-=======
-    private String text;
-    private int id;
 
-    @Override
-    public String toString() {
-        return "Text:" + text + "Id: " + id;
-    } 
->>>>>>> b7e8c91fc171862a92153f153799acd9a3ad17f9
 }
