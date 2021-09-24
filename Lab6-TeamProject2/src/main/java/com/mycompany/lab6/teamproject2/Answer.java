b73ee1f0befa4dba5534871a827b2604cbb34f7c
Thomas Kolar <thomas.kolar@go.tarleton.edu>
2021-09-23 22:47:56 -0500

    Added comments and tweaked output file

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
@@ -1,17 +1,33 @@
 package com.mycompany.lab6.teamproject2;
 
 
-public class Answer {
+public class Answer { //Stores an answer along with the question id
     private String text;
     private int questionId;
 
-    public Answer(String text, int questionId) {
+    public Answer(String text, int questionId) { //constructor for answer
         this.text = text;
+        this.questionId = questionId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
         this.questionId = questionId;
     }
     
     @Override
-    public String toString() {
+    public String toString() { //toString to print text and questionId
         return "Text: " + text + ", questionId: " + questionId;
     }
 }
