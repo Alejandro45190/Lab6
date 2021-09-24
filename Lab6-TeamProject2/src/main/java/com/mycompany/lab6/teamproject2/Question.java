37d8de3f091c77a06e8545a52955ca7af7568150
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-23 19:00:37 -0500

    Attribute id and toString added in Question class

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
@@ -1,5 +1,10 @@
 package com.mycompany.lab6.teamproject2;
 
 public class Question {
-    
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Id: " + id;
+    }    
 }
