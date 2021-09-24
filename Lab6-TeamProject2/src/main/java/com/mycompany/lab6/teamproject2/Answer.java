63a425f1bfc6d27af279a0c2df22e94a72525760
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-23 19:58:27 -0500

    Add questionId attribute

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
@@ -2,5 +2,11 @@ package com.mycompany.lab6.teamproject2;
 
 
 public class Answer {
-    
+    private String text;
+    private int questionId;
+
+    @Override
+    public String toString() {
+        return "Text: " + text + ", questionId: " + questionId;
+    }
 }
