b7e8c91fc171862a92153f153799acd9a3ad17f9
Thomas Kolar <thomas.kolar@go.tarleton.edu>
2021-09-23 19:45:38 -0500

    Modified Main and Answer

--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Answer.java
@@ -2,5 +2,10 @@ package com.mycompany.lab6.teamproject2;
 
 
 public class Answer {
-    
+    private String text;
+
+    @Override
+    public String toString() {
+        return "Text:" + text;
+    }
 }
--- Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
+++ Lab6-TeamProject2/src/main/java/com/mycompany/lab6/teamproject2/Question.java
@@ -1,21 +1,11 @@
 package com.mycompany.lab6.teamproject2;
 
 public class Question {
-<<<<<<< HEAD
     private String text;
-
-    @Override
-    public String toString() {
-        return "Text:" + text;
-    }
-    
-    
-=======
     private int id;
 
     @Override
     public String toString() {
-        return "Id: " + id;
-    }    
->>>>>>> 37d8de3f091c77a06e8545a52955ca7af7568150
+        return "Text:" + text + "Id: " + id;
+    } 
 }
